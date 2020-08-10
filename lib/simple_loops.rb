d0dbec8b89b6d0c8fc8c5eaf7ace50e64c24d8d8
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-10 02:25:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -20,9 +20,10 @@ end
 
 def return_string_array(array)
 count = 0
-  while count<array.length do
-    array[count].to_s
+conv_array =[]
+  while count < array.length do
+    conv_array.push(array[count].to_s)
     count+=1
   end
-  array
+  conv_array
 end
